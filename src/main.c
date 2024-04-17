23d61007b66f388a72f2a82a82f4a2dd44c7ff4f
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-04-17 19:25:00 +0530

    feat: wire main to server event loop

--- src/main.c
+++ src/main.c
@@ -2,6 +2,10 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <liburing.h>
+#include "conn/conn.h"
+#include "ring/ring.h"
+#include "server/server.h"
 
 #define DEFAULT_PORT 8080
 #define WWW_DIR      "./www"
@@ -13,9 +17,16 @@ int main(int argc, char *argv[])
     int port = DEFAULT_PORT;
     if (argc > 1) port = atoi(argv[1]);
 
-    printf("starting io_uring splice server on :%d  www=%s\n", port, WWW_DIR);
+    struct io_uring ring;
+
+    int listen_fd = server_setup_listen_socket(port);
+    ring_init(&ring);
+    int www_dirfd = server_open_www_dir(WWW_DIR);
+    conn_pool_init();
+
+    printf("listening on :%d  www=%s\n", port, WWW_DIR);
     fflush(stdout);
 
-    /* TODO: io_uring init, listen socket, accept/recv/splice event loop */
+    server_event_loop(&ring, listen_fd, www_dirfd);
     return 0;
 }
